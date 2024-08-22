dbf4eecfffc77894c485d2fedea3adbc46b6f0ce
rizalshmbg <sihombingrizal18@gmail.com>
2024-08-22 15:46:51 +0700

    complete function delete feedback

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,31 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<script>
+	import '../../src/app.css'
+	import FeedbackList from '../components/FeedbackList.svelte'
+
+	let feedback = [
+		{
+			id: 1,
+			rating: 10,
+			text: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. consequuntur vel vitae commodi alias voluptatem est voluptatum ipsa quae.',
+		},
+		{
+			id: 2,
+			rating: 9,
+			text: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. consequuntur vel vitae commodi alias voluptatem est voluptatum ipsa quae.',
+		},
+		{
+			id: 3,
+			rating: 8,
+			text: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. consequuntur vel vitae commodi alias voluptatem est voluptatum ipsa quae.',
+		},
+	]
+
+	const deleteFeedback = (e) => {
+		const itemId = e.detail
+		feedback = feedback.filter((item) => item.id !== itemId)
+	}
+</script>
+
+<main class="container">
+	<FeedbackList {feedback} on:delete-feedback={deleteFeedback} />
+</main>
