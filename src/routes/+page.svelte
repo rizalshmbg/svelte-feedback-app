25e5ccfdd4a9d6adc14cbc4bd41d100922bcac47
rizalshmbg <sihombingrizal18@gmail.com>
2024-08-23 15:43:10 +0700

    finish addFeedback

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -26,6 +26,11 @@
 	$: average =
 		feedback.reduce((a, { rating }) => a + rating, 0) / feedback.length
 
+	const addFeedback = (e) => {
+		const newFeedback = e.detail
+		feedback = [newFeedback, ...feedback]
+	}
+
 	const deleteFeedback = (e) => {
 		const itemId = e.detail
 		feedback = feedback.filter((item) => item.id !== itemId)
@@ -33,7 +38,7 @@
 </script>
 
 <main class="container">
-	<FeedbackForm />
+	<FeedbackForm on:add-feedback={addFeedback} />
 	<FeedbackStats {count} {average} />
 	<FeedbackList {feedback} on:delete-feedback={deleteFeedback} />
 </main>
